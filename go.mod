0eea0a36d88d8194f518346ef774054023377546
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2024-10-17 15:07:37 +0200

    fix: update module declaration

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/xi/go-tinyqr
+module github.com/hashmapsdata2value/go-tinyqr
 
 go 1.13
--- qrcode/main.go
+++ qrcode/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	qrcode "github.com/xi/go-tinyqr"
+	qrcode "github.com/hashmapsdata2value/go-tinyqr"
 )
 
 func main() {
